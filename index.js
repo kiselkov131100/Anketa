3952e02a9fb615c25ecec5ed79681824e167da56
Ivan Kiselkov <ivan131100@icloud.com>
2022-01-26 22:41:14 +0300

    added validation

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
 "use strict";
 
+let repeat = () => alert("Введите корректные данные");
+
 let userSurname = prompt("Ваша фамилия?");
+while (!isNaN(userSurname)) {
+  if (!isNaN(userSurname)) {
+    repeat();
+  }
+  userSurname = prompt("Ваша фамилия?");
+}
+
 let userName = prompt("Ваше имя?");
+while (!isNaN(userName)) {
+  if (!isNaN(userName)) {
+    repeat();
+  }
+  userName = prompt("Ваше имя?");
+}
+
 let userPatronymic = prompt("Ваше отчество?");
+while (!isNaN(userPatronymic)) {
+  if (!isNaN(userPatronymic)) {
+    repeat();
+  }
+  userPatronymic = prompt("Ваше отчество?");
+}
+
 let age = prompt("Ваш возраст?");
-let yourGender = confirm("Ваш пол - мужской?");
-const menPension = 65;
-const womenPension = 55;
+while (!Number(age)) {
+  if (!Number(age)) {
+    repeat();
+  }
+  age = prompt("Ваш возраст?");
+}
+
 let ageDays = age * 365;
 let age5years = +age + 5;
+
+const MenPension = 65;
+const WomenPension = 55;
+
+let yourGender = confirm("Ваш пол - мужской?");
 let gender = yourGender ? "мужской" : "женский";
+
 let pension;
 
-if ((age >= 65 && yourGender) || (age >= 55 && (yourGender = false))) {
+if (
+  (age >= MenPension && gender === "мужской") ||
+  (age >= WomenPension && gender === "женский")
+) {
   pension = "да";
 } else {
   pension = "нет";
 }
 
-alert(` Ваше ФИО: ${userSurname}  ${userName}  ${userPatronymic}
-Ваш возраст в годах: ${age}
-Ваш возраст в днях: ${ageDays}
-Через 5 лет вам будет: ${age5years}
-Ваш пол: ${gender}
-Вы на пенсии: ${pension}
+alert(` 
+  Ваше ФИО: ${userSurname}  ${userName}  ${userPatronymic}
+  Ваш возраст в годах: ${age}
+  Ваш возраст в днях: ${ageDays}
+  Через 5 лет вам будет: ${age5years}
+  Ваш пол: ${gender}
+  Вы на пенсии: ${pension}
 `);
 
 // Спросить у пользователя строку и посчитать количество русских гласных букв, отобразить через alert();
